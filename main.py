ec1405cb1f279d6a8de8095c7036c9573d706b47
Letícia Borba <leticiaborba.b@gmail.com>
2022-08-31 17:31:37 -0300

    feat: added the main loop

--- main.py
+++ main.py
@@ -40,53 +40,64 @@ def prettify_recipe(recipe_name, base_ingredient, final_quantity, base_unit, out
 
 
 def main():
-    clear()
-    print(art)
-    print("\n  This is a recipe proportion calculator, welcome.\n"
-          "\n  Here you can create a new recipe,\n"
-          "  save it for future use\n"
-          "  or read an existing file.\n")
-    while True:
-        starter_input = input("  Do you want to fill in the ingredients\n"
-                        "  or load a csv file? (use 'fill' or 'load')\n  ").lower()
-        if starter_input == "fill":
-            clear()
-            recipe = NewRecipe()
-            clear()
-            break
-        elif starter_input == "load":
-            clear()
-            recipe = ReadRecipe()
-            clear()
-            break
-        else:
-            print("        -- Invalid option, please try again. --\n")
+    is_calc_running = True
+    while is_calc_running:
+        clear()
+        print(art)
+        print("\n  This is a recipe proportion calculator, welcome.\n"
+            "\n  Here you can create a new recipe,\n"
+            "  save it for future use\n"
+            "  or read an existing file.\n")
+        while True:
+            starter_input = input("  Do you want to fill in the ingredients\n"
+                            "  or load a csv file? (use 'fill' or 'load')\n  ").lower()
+            if starter_input == "fill":
+                clear()
+                recipe = NewRecipe()
+                clear()
+                break
+            elif starter_input == "load":
+                clear()
+                recipe = ReadRecipe()
+                clear()
+                break
+            else:
+                print("        -- Invalid option, please try again. --\n")
 
-    print("\n  What is the ingredient that you want to base the proportion on?")
-    ingredients = recipe.recipe_data()["Ingredient"].to_list()
-    while True:
-        base_ingredient = input(f"  {ingredients}: ").capitalize()
-        if base_ingredient in ingredients:
-            ingredient_row = recipe.recipe_data().loc[recipe.recipe_data()["Ingredient"] == base_ingredient]
-            base_quantity = float(ingredient_row["Quantity"])
-            base_unit = ingredient_row["Unit"].to_string(index=False).strip()
-            break
-        else:
-            print("        -- Invalid ingredient, please try again. --\n")
+        print("\n  What is the ingredient that you want to base the proportion on?")
+        ingredients = recipe.recipe_data()["Ingredient"].to_list()
+        while True:
+            base_ingredient = input(f"  {ingredients}: ").capitalize()
+            if base_ingredient in ingredients:
+                ingredient_row = recipe.recipe_data().loc[recipe.recipe_data()["Ingredient"] == base_ingredient]
+                base_quantity = float(ingredient_row["Quantity"])
+                base_unit = ingredient_row["Unit"].to_string(index=False).strip()
+                break
+            else:
+                print("        -- Invalid ingredient, please try again. --\n")
 
-    while True:
-        try:
-            user_quantity = float(input(f"\n  How much (in '{base_unit}') do you have of {base_ingredient}? "))
-            break
-        except ValueError:
-            print("        -- Invalid quantity, please try again. --")
+        while True:
+            try:
+                user_quantity = float(input(f"\n  How much (in '{base_unit}') do you have of {base_ingredient}? "))
+                break
+            except ValueError:
+                print("        -- Invalid quantity, please try again. --")
 
-    proportion = proportion_calculator(base_quantity, user_quantity)
-    output_recipe = recipe_calculator(recipe, proportion, ingredients, base_ingredient)
-    print(f"\n  Thank you, here is your newly calculated recipe:")
-    prettify_recipe(recipe.name, base_ingredient, user_quantity, base_unit, output_recipe)
+        proportion = proportion_calculator(base_quantity, user_quantity)
+        output_recipe = recipe_calculator(recipe, proportion, ingredients, base_ingredient)
+        print(f"\n  Thank you, here it is:")
+        prettify_recipe(recipe.name, base_ingredient, user_quantity, base_unit, output_recipe)
 
-    input("\n        -- Press enter to exit. --")
+        while True:
+            continue_input = input("\n  Enjoy your recipe!"
+                                "\n  Do you want to try another one? (y/n) ").lower()
+            if continue_input == "y":
+                break
+            elif continue_input == "n":
+                is_calc_running = False
+                break
+            else:
+                print("        -- Invalid option, please try again. --")
 
 
 main()
